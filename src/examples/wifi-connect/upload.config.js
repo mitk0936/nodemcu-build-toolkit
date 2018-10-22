78d3c46efc581ce3ebe48dc8a0fadab39e7cae6f
Dimitar <dmihaylov93@gmail.com>
2018-10-22 15:04:20 +0300

    Added examples dependency library

--- src/examples/wifi-connect/upload.config.js
+++ src/examples/wifi-connect/upload.config.js
@@ -2,7 +2,7 @@ module.exports = {
   connectionDelay: 200,
   baud: 115200,
   source: {
-    libs: ['../lib/moon_saga.lua'],
+    libs: ['../lib/moon-saga/src/moon_saga.lua'],
     scripts: './app/*.lua',
     static: './static/**/*.{json,html}'
   }
